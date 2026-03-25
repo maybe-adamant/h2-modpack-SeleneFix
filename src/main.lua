8bdba8b80954213600fe5f60ea6475be0c435524
maybe-adamant <maybe.adamant@gmail.com>
2026-03-25 16:23:12 -0400

    changing discovery flag from bool to string. preparing to moving core to a framework

--- src/main.lua
+++ src/main.lua
@@ -53,7 +53,7 @@ public.definition = {
     tooltip  = "Aspect of Selene properly registers its hex so you get offered PoS directly. Skyfall is full moonglow.",
     default  = true,
     dataMutation = true,
-    modpackModule = true,
+    modpack = "h2-modpack",
 }
 
 -- =============================================================================
